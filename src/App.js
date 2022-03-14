ca2f4afc8d513a623d35a67ccf866340595b9017
SanjayJatti <jattisanjay.r@gmail.com>
2022-03-14 11:58:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// import './App.css';
-import { Home } from "./Pages/Home"
-import MockApi from "./mock-api"
+import './App.css';
+import {Home} from "./Pages/Home"
 
 function App() {
   return (
     <div className="App">
+      <Home/>
      
-      <MockApi/>
+   
 
     </div>
   );
--- src/mock-api.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Mockman from "mockman-js"
-export default function Mockapi() {
-  return (
-    <div className="MockApi">
-        <Mockman/>
-    </div>
-  )
-}
